57f11683530a02497c0db710f4da666844b192f8
hassan-abdallah <42123944+hassan-abdallah@users.noreply.github.com>
2021-08-04 12:31:50 -0400

    Update montecarlo_sim_block.R

--- R/montecarlo_sim_block.R
+++ R/montecarlo_sim_block.R
@@ -3,9 +3,8 @@
 #' This function computes a p-value using a monte carlo simulation with block sampling.  
 #' @param distancemat A distance matrix. The output of distance_mat()
 #' @param grouplabels A set of labels(indices) for one of the two groups in experiment. 
+#' @param levels   Two-column matrix encoding index information for two-level point-cloud grouping. First column is whole-block shuffling partition and second column is within-block shuffling partition. 
 #' @param N	      The number of permutations to be performed. 
-#' @param blocks      The number of exchangeable blocks. Usually the number of epochs in an experiment.
-#' @param blocksize   The size of each block, only give one value. 
 #' @keywords 
 #' @export
 #' @examples
@@ -13,103 +12,88 @@
 
 
 
-montecarlo_sim_block <- function(distancemat, grouplabels, N,blocks,blocksize){
-
-
+montecarlo_sim_block <- function(distancemat, grouplabels,levels, N){
+  
+  
   dist <- vector()
-
-
+  
+  
   F_observed <- jointloss(distancemat,grouplabels)
-
+  
   dist[1] <- F_observed
   
-
+  
   Z <- 1
- 
-
+  
+  
   n1 <- length(grouplabels)
   
-
-  n2 <- nrow(distancemat) - length(grouplabels)
- 
-
-  total <- n1+n2
- 
-
-  for(i in 1:N-1){
   
-
-    segs <- sample(1:(blocks/2),blocks/4)
-
-
+  n2 <- nrow(distancemat) - length(grouplabels)
+  
+  
+  total <- n1+n2
+  
+  
+  for(i in 1:N-1){
+    
+    
+    
     newlabels <- vector()
-   
-
-    for(i in segs){
-
-      if(i == 1){
-
-        newlabels <- c(newlabels,1:(blocksize))
-
-      } else {
-
-        newlabels <- c(newlabels,((i-1)*blocksize+1):(((i-1)*blocksize)+(blocksize)))
-
-      }
-
-    }
-
-
-    segs <- sample(((blocks/2)+1):blocks,blocks/4)
     
-
-    for(i in segs){
-
-
-      if(i == 1){
-
-        newlabels <- c(newlabels,1:(blocksize))
-
-      } else {
-
-        newlabels <- c(newlabels,((i-1)*blocksize+1):(((i-1)*blocksize)+(blocksize)))
-
-      }
-
-    }
-
     
-
-    F_sim <- jointloss(distancemat, newlabels)
-
-
-    dist[i+1] <- F_sim
-
-
-    if(F_sim <= F_observed) {
-
-     
-
-      Z <- Z+1
-
+    lev_ind <- list()
+    
+    for(i in 1:max(levels[,2])){
       
-
+      lev_ind[[i]] <- unique(levels[which(levels[,2] == i),1])
+      
     }
-
-   
-
     
-
+    new_label_ind <- c()
+    for(i in 1:max(levels[,2])){
+      
+      len <- length(lev_ind[[i]])
+      
+      new_label_ind <- c(new_label_ind,sample(lev_ind[[i]],len/2))
+      
+    }
+    
+    newlabels <- which(levels[,1] %in% new_label_ind)
+    
+    #print(newlabels)
+    
+    F_sim <- jointloss(distancemat, newlabels)
+    
+    
+    dist[i+1] <- F_sim
+    
+    
+    if(F_sim <= F_observed) {
+      
+      
+      
+      Z <- Z+1
+      
+      
+      
+    }
+    
+    
+    
+    
+    
   }
   
-
-  p_value <- Z/(N+1)
-
-
-  return(p_value)
-
   
-
+  p_value <- Z/(N+1)
+  
+  
+  return(p_value)
+  
+  
+  
 }
+
 
  
